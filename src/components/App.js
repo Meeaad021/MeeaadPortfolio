8819d7a76052ee2adea40eb3b0e381dda0492b6f
JonSneeu02 <3683628@myuwc.ac.za>
2024-03-09 20:38:45 +0200

    start bar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <div className="grey-bar"></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/StartMenu.js
@@ -0,0 +1,14 @@
+// StartMenu.js
+
+import React from 'react';
+import './StartMenu.css';  // Correct the filename case
+
+const StartMenu = () => {
+  return (
+    <div className="start-menu">
+      {/* Start menu content goes here */}
+    </div>
+  );
+};
+
+export default StartMenu;
