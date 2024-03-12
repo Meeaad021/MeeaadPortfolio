3ac1b8716c6c70660da17e784d81a13106b63d0f
JonSneeu02 <3683628@myuwc.ac.za>
2024-03-12 17:51:51 +0200

    Added volume image to taskbar

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/Taskbar.css';
 import win98StartMenuImage from '../images/Win98StartMenu.webp';
+import Win98Volume from '../images/Win98Volume.png';
 
 const Taskbar = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+ 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+  return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="taskbar">
       <button className="button-start-menu">
         <img src={win98StartMenuImage} alt="Start Menu Icon" />
         Start
       </button>
-      <div className="tasks"></div>
       <div className="taskbar-divider"></div>
-      <div className="taskbar-time">
-        <div className="tray-icons"></div>
+      <div className="tasks">
+        <img src={Win98Volume} alt="Volume Icon" className="small-image volume-icon" />/>
+        <div className="taskbar-time">
+          {currentTime.toLocaleTimeString()}
+        </div>
       </div>
     </div>
   );
