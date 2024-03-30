15f3cb0beed70e58957173507c7b0dd11992808f
JonSneeu02 <3683628@myuwc.ac.za>
2024-03-30 10:04:05 +0200

    Page 2

--- src/components/AboutMeContent.js
+++ src/components/AboutMeContent.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/AboutMeContent.css';
 import CloseIcon from '../images/Closeicon.png';
 
 const AboutMeContent = ({ onClose }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 3;
+
   const handleClose = () => {
-    onClose(); // Call onClose function passed as a prop
+    onClose();
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(prevPage => prevPage + 1);
+    } else {
+      setCurrentPage(1);
+    }
   };
 
   return (
@@ -18,13 +29,40 @@ const AboutMeContent = ({ onClose }) => {
         </div>
       </div>
       <div className="about-me-content">
-        <p>
-          As a BCom graduate with a major in Information Systems, I offer a diverse skill set, blending creativity,
-          strategic thinking, and a commitment to continuous improvement. Transitioning from content writing to data
-          capture, my primary aspiration is to excel in software development. My creative problem-solving and audience
-          engagement skills, combined with meticulous attention to detail, can add significant value to your company.
-        </p>
+        <div className={`page${currentPage}`}>
+          {currentPage === 1 && (
+            <div>
+              <p>
+                As a BCom graduate with a major in Information Systems, I offer a diverse skill set, blending creativity,
+                strategic thinking, and a commitment to continuous improvement. Transitioning from content writing to data
+                capture, my primary aspiration is to excel in software development. My creative problem-solving and audience
+                engagement skills, combined with meticulous attention to detail, can add significant value to your company.
+              </p>
+            </div>
+          )}
+          {currentPage === 2 && (
+            <div>
+              <p>
+              SKILLS
+              Languages: Python, Panda, Selenium, JavaScript, TypeScript
+              Framework: 
+              Environments: 
+
+
+
+              </p>
+            </div>
+          )}
+          {currentPage === 3 && (
+            <div>
+              <p>
+                This is the content of Page 3.
+              </p>
+            </div>
+          )}
+        </div>
       </div>
+      <button className="btn" onClick={nextPage}>Next Page</button>
     </div>
   );
 };
